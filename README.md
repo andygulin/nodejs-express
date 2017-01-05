35bcce1d6c9e06f65b9e70f4835de305b0053c86
andygulin <andygulin@hotmail.com>
2017-01-05 14:34:30 +0800

    ...

---
+++ README.md
@@ -0,0 +1,45 @@
+#### NodeJS Express Rest Sample
+
+### 运行
+```
+npm install
+npm start 3000
+```
+
+##### 返回文本
+    URL : /hello
+    METHOD : GET
+
+##### 返回所有用户
+    URL : /users/all
+    METHOD : GET
+    
+##### 根据ID返回用户
+    URL : /users/get/{id}
+    METHOD : GET
+    
+##### 根据ID删除用户
+    URL : /users/delete/{id}
+    METHOD : DELETE
+    
+##### 插入用户
+    URL : /users/insert
+    METHOD : PUT
+    BODY : {
+               "name":"insert user",
+               "age":123,
+               "address":"insert address"
+           }
+    Content-Type : application/json
+    
+##### 更新用户
+    URL : /users/update
+    METHOD : PUT
+    BODY : {
+                "id":1,
+                "name":"update user",
+                "age":123,
+                "address":"update address"
+              }
+    Content-Type : application/json
+    
---
+++ routes/users.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+var dbConfig = require('../conf/dbConfig');
+var pool = mysql.createPool(dbConfig.mysql);
+
+router.get('/users/get/:id',
+function(req, res) {
+    pool.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM user WHERE id=?', [req.params.id],
+        function(err, rows, fields) {
+            if (err) throw err;
+            res.json({
+                'success': true,
+                'ret': rows
+            });
+            connection.release();
+        });
+    });
+});
+
+router.get('/users/all',
+function(req, res) {
+    pool.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM user',
+        function(err, rows, fields) {
+            if (err) throw err;
+            res.json({
+                'success': true,
+                'ret': rows
+            });
+            connection.release();
+        });
+    });
+});
+
+router.delete('/users/delete/:id',
+function(req, res) {
+    pool.getConnection(function(err, connection) {
+        connection.query('DELETE FROM user WHERE id=?', [req.params.id],
+        function(err, rows, fields) {
+            if (err) throw err;
+            res.json({
+                'success': true,
+                'ret': rows.affectedRows
+            });
+            connection.release();
+        });
+    });
+});
+
+router.put('/users/insert',
+function(req, res) {
+    req.accepts('application/json');
+    var body = req.body;
+    pool.getConnection(function(err, connection) {
+        connection.query('INSERT INTO user(name,age,address,createdAt) VALUES(?,?,?,?)', [body.name, body.age, body.address, new Date()],
+        function(err, rows, fields) {
+            if (err) throw err;
+            res.json({
+                'success': true,
+                'ret': rows.insertId
+            });
+            connection.release();
+        });
+    });
+});
+
+router.put('/users/update',
+function(req, res) {
+    req.accepts('application/json');
+    var body = req.body;
+    pool.getConnection(function(err, connection) {
+        connection.query('UPDATE user SET name=?,age=?,address=? WHERE id=?', [body.name, body.age, body.address, body.id],
+        function(err, rows, fields) {
+            if (err) throw err;
+            res.json({
+                'success': true,
+                'ret': rows.affectedRows
+            });
+            connection.release();
+        });
+    });
+});
+
+module.exports = router;
