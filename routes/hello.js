6f86e15c40edf625d380c632679f0c63ddbb8239
andygulin <andygulin@hotmail.com>
2017-05-24 09:36:31 +0800

    ...

--- routes/hello.js
+++ routes/hello.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/hello',
-function(req, res) {
-    res.send('Hello World~');
-});
+    function (req, res) {
+        res.send('Hello World~');
+    });
 
 module.exports = router;
--- routes/qrcode.js
+++ routes/qrcode.js
@@ -4,11 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/qrcode/create',
-function(req, res) {
-    var text = req.query.text;
-    var img = qr.image(text,{'size' : 15, 'margin' : 1});
-	res.writeHead(200, {'Content-Type': 'image/png'});
-	img.pipe(res);
-});
+    function (req, res) {
+        var text = req.query.text;
+        var img = qr.image(text, {'size': 15, 'margin': 1});
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        img.pipe(res);
+    });
 
 module.exports = router;
